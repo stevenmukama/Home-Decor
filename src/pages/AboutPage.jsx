1069bc5578700a8d1136d690b0cfcf721d1a4e59
Mukama Steven <stevenmukama1@gmail.com>
2024-05-29 17:15:18 +0200

    add more changes

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,14 +1,18 @@
-// src/pages/AboutPage.js
 import PageContent from '../components/PageContent';
+import ColoredSvg from '../components/ColoredSvg';
 
 const AboutPage = () => {
 	return (
-		<PageContent
-			title='About Page'
-			color='bg-red-500'
-			nextPage='/contact'
-			nextPageText='Go to Contact Page'
-		/>
+		<div className='relative min-h-screen'>
+			<ColoredSvg color='#FF0000' />
+			<div className='relative z-10'>
+				<PageContent
+					title='About Page'
+					nextPage='/contact'
+					nextPageText='Go to Contact Page'
+				/>
+			</div>
+		</div>
 	);
 };
 
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,13 +1,18 @@
+import ColoredSvg from '../components/ColoredSvg';
 import PageContent from '../components/PageContent';
 
 const ContactPage = () => {
 	return (
-		<PageContent
-			title='Contact Page'
-			color='bg-green-500'
-			nextPage='/services'
-			nextPageText='Go to Services Page'
-		/>
+		<div className='relative min-h-screen'>
+			<ColoredSvg color='#00FF00' />
+			<div className='relative z-10'>
+				<PageContent
+					title='Contact Page'
+					nextPage='/services'
+					nextPageText='Go to Services Page'
+				/>
+			</div>
+		</div>
 	);
 };
 
