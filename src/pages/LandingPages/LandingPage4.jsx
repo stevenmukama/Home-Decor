902caa8e0e31432d2d7a1c3c652de738c3b0dbbb
Mukama Steven <stevenmukama1@gmail.com>
2024-05-31 11:41:43 +0200

    added more changes

--- src/pages/LandingPages/LandingPage4.jsx
+++ src/pages/LandingPages/LandingPage4.jsx
@@ -19,6 +19,15 @@ const LandingPage4 = () => {
 				images={images}
 				imageProp={'/imagelink4.webp'}
 				color='bg-[#E7E3E1]'
+				buttonColor1='bg-[#978F89]'
+				bgColor1='bg-[#978F89]'
+				textCard1='text-[#7B7979]'
+				numberColor1='text-[#96806C]'
+				buttonColor2='bg-[#8B7E77]'
+				bgColor2='bg-[#D6CFCB]'
+				bgColor3='bg-[#615853]'
+				subscribeBgColor1='bg-[#8B7E77]'
+				FooterItemColor='bg-[#615853]'
 			/>
 		</div>
 	);
